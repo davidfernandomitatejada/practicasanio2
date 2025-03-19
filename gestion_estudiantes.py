878e5e422c8640d98374afc4f70feee9a9f566b0
davidfernandomitatejada <mitatejadadavidfernando@gmail.com>
2025-03-19 09:15:07 -0400

    adicion de funciones de promedio

--- gestion_estudiantes.py
+++ gestion_estudiantes.py
@@ -1,20 +1,23 @@
 class Estudiante:
     estudiantes_matriculados = {} 
 
-    def __init__(self, nombre, edad, grado, promedio):
+    def __init__(self, nombre, edad, grado, notas):
         self.nombre = nombre
         self.edad = edad
         self.grado = grado
-        self.promedio = promedio  
+        self.notas = notas
+        self.promedio = sum(notas)/len(notas)  
 
         Estudiante.estudiantes_matriculados[nombre] = {
             "edad": edad,
             "grado": grado,
-            "promedio": promedio
+            "notas": notas,
+            "promedio":self.promedio 
         }
 
     def __str__(self):
         return f"Nombre: {self.nombre}, Edad: {self.edad}, Grado: {self.grado}, Promedio: {self.promedio}"
+    
 
     @classmethod
     def mostrar_estudiantes(cls):
@@ -27,8 +30,15 @@ class Estudiante:
         )
 
     @staticmethod
-    def esta_aprobado(promedio):
-        print("Aprobado" if promedio >= 51 else "Reprobado")
+    def esta_aprobado(nombre):
+        if nombre in Estudiante.estudiantes_matriculados:
+            if Estudiante.estudiantes_matriculados[nombre]["promedio"] >= 60:
+                print(f"El estudiante {nombre} ha sido aprobado")
+            else:
+                print(f"El estudiante {nombre} no ha sido aprobado")
+        else:
+            print(f"El estudiante {nombre} no está registrado")
+
 
     @classmethod
     def contar_estudiantes(cls):
@@ -37,8 +47,10 @@ class Estudiante:
     @classmethod
     def actualizar_notas(cls, nombre, nuevas_notas):
         if nombre in cls.estudiantes_matriculados:
-            cls.estudiantes_matriculados[nombre]["promedio"] = nuevas_notas
-            print(f"El promedio de {nombre} ha sido actualizado a {nuevas_notas}")
+            nuevo_promedio = sum(nuevas_notas) / len(nuevas_notas)
+            cls.estudiantes_matriculados[nombre]["notas"] = nuevas_notas
+            cls.estudiantes_matriculados[nombre]["promedio"] = nuevo_promedio
+            print(f"El promedio de {nombre} ha sido actualizado a {nuevo_promedio}")
         else:
             print(f"No se encontró al estudiante {nombre}")
 
@@ -46,7 +58,7 @@ class Estudiante:
     def mejor_promedio(cls):
         if not cls.estudiantes_matriculados:
             print("No hay estudiantes registrados.")
-            return
+            return 
 
         mejor_nombre = None
         mejor_promedio = -1  
@@ -58,16 +70,16 @@ class Estudiante:
 
         print(f"El mejor promedio es {mejor_promedio}, de {mejor_nombre}")
 
-estudiante1 = Estudiante("ALex Moscoso", 16, "Noveno", 80)        
-estudiante2 = Estudiante("Ana López", 12, "Sexto", 40)
-estudiante3 = Estudiante("Carlos Gómez", 17, "Décimo", 60)
+estudiante1 = Estudiante("ALex Moscoso", 16, "Noveno", [80,60,70])        
+estudiante2 = Estudiante("Ana López", 12, "Sexto", [40,60,67])
+estudiante3 = Estudiante("Carlos Gómez", 17, "Décimo", [55,80,70])
 
 print(Estudiante.mostrar_estudiantes())
 
-Estudiante.esta_aprobado(estudiante1.promedio)
+Estudiante.esta_aprobado("Ana López")
 
 Estudiante.contar_estudiantes()
 
-Estudiante.actualizar_notas("Ana López", 70)
+Estudiante.actualizar_notas("Ana López",[70,60,80] )
 
 Estudiante.mejor_promedio()
